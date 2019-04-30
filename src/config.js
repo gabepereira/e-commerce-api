3ed43725aa521e528e08934e0d98c30fe27b1534
Gabriel Pereira <gabriel6562014@gmail.com>
2019-04-30 02:57:20 -0300

    Sensitive information removed.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = '3368f0eb1f24cc0228b4938f965e7e17';
+global.SALT_KEY = 'Put your own key here.';
 global.EMAIL_TMPL = '<strong>{0}</strong>';
 
-const user = 'mrdoge';
-const pass = 'doge666';
+const user = 'user';
+const pass = 'pass';
 const db = 'mongodb+srv://' + user + ':' + pass + '@cluster0-f0whx.azure.mongodb.net/test?retryWrites=true';
 
 module.exports = {
